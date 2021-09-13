fb13d663a67885512fb55c0ea62f91ab52f41173
Jeremiah Jacob <jaykon4me@gmail.com>
2021-09-13 07:37:45 +0100

    initial commit

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('Hello');
+
+// alert('Hello this is jay');
+
+// How to write a comment inline
+
+// Varibles
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt('What is your age?');
+document.getElementById('someText').innerHTML = age;
