975f7ad61c28d7b1402b804661987aa060329bf3
Jeremiah Jacob <jaykon4me@gmail.com>
2021-09-13 08:31:39 +0100

    numbers in javascript

--- home.js
+++ home.js
@@ -10,6 +10,26 @@ console.log(b);
 
 var someNumber = 45;
 console.log(someNumber);
+//Manipulate DOM with JavaScript
+/* ... It's just a fancy way of saying
+change HTML with JavaScript*/
 
-var age = prompt('What is your age?');
-document.getElementById('someText').innerHTML = age;
+//var age = prompt('What is your age?');
+//document.getElementById('someText').innerHTML = age;
+
+// Numbers in JavaScript
+var num1 = 10;
+
+// Increament num1 by 1
+num1++;
+
+// Decreament num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *, remainder %
+console.log(num1 % 6);
+
+// Increament/decreament by 20
+num1 += 20;
+console.log(num1);
